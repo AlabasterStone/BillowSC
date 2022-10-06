023cb87969baaf963bc1b9bfb1d8fba43ebefc23
LuminousWL <TheWavyLines@outlook.com>
2022-10-06 18:18:26 +0800

    添加注释 为object类添加compile方法 blockASMParser重命名为blockASMCore.py 添加standard compiler类

--- blockASMCore.py
+++ blockASMCore.py
@@ -4,4 +4,153 @@ Copyright (c) Luminous Zhang 2022
 All right reserved
 '''
 
-from blockASMParser import *
+import logging
+import json
+from zipfile import ZipFile
+from typing import Any, Dict, List
+
+# set logger
+logging.basicConfig(level=logging.DEBUG)
+
+class scProjectMonitor:
+    def __init__(self, id: str, mode: str, opcode: str, params: dict, spriteName: Any, value: int, visible: bool, sliderMin: int, sliderMax: int, isDiscrete: bool) -> None:
+        self.id = id
+        self.mode = mode
+        self.opcode = opcode
+        self.params = params
+        self.spriteName = spriteName
+        self.value = value
+        self.visible = visible
+        self.sliderMin = sliderMin
+        self.sliderMax = sliderMax
+        self.isDiscrete = isDiscrete
+
+    # TODO: compile monitor
+    # return C++ code
+    def compile(self) -> str:
+        pass
+
+class scProjectBlock:
+    def __init__(self, blockID: str, opcode: str, nextID: str, parentID: str, inputs: dict, topLevel: bool) -> None:
+        self.blockID = blockID
+        self.opcode = opcode
+        self.nextID = nextID
+        self.parentID = parentID
+        self.inputs = inputs
+        self.topLevel = topLevel
+
+    # TODO: compile block
+    # return C++ code
+    def compile(self) -> str:
+        pass
+
+class scProjectSprite:
+    def __init__(self, isStage: bool, name: str, variables, lists, broadcasts, blocks: List[scProjectBlock], currentCostume: int, costumes, sounds, volume: int, layerOrder: int, tempo: int, videoTransparency: int, videoState: str) -> None:
+        self.isStage = isStage
+        self.name = name
+        self.varibles = variables
+        self.lists = lists
+        self.broadcasts = broadcasts
+        self.blocks = blocks
+        self.currentCostume = currentCostume
+        self.costumes = costumes
+        self.sounds = sounds
+        self.volume = volume
+        self.layerOrder = layerOrder
+        self.tempo = tempo
+        self.videoTransparency = videoTransparency
+        self.videoState = videoState
+
+    # TODO: compile sprite
+    # return C++ code
+    def compile(self) -> str:
+        pass
+
+class scProjectTarget:
+    def __init__(self, sprites: List[scProjectSprite]) -> None:
+        self.sprites = sprites
+    
+    # TODO: compile object
+    # return C++ code
+    def compile(self) -> str:
+        pass
+
+class scProjectObject:
+    def __init__(self, targets: scProjectTarget, monitors, extensions: list) -> None:
+        self.targets = targets
+        self.monitors = monitors
+        self.extensions = extensions
+
+    # TODO: compile object
+    # return C++ code
+    def compile(self) -> str:
+        pass
+
+# It takes a .sb3 file and parses it into a Python object
+class blockASMProject:
+    def __init__(self, scFilePath: str, scFileTargetDir: str) -> None:
+        self.scFilePath = scFilePath
+        self.scFileTargetDir = scFileTargetDir
+        self.scJsonFilePath = scFileTargetDir + "/project.json"
+        self.unpackZip()
+
+    def unpackZip(self):
+        try:
+            ZipFile(self.scFilePath, "r").extractall(self.scFileTargetDir)
+        except Exception as e:
+            logging.error(e)
+
+    def parseSb3Json(self) -> scProjectObject:
+        with open(self.scJsonFilePath, "r", encoding="utf-8") as scJsonFileHandle:
+            scJsonObject = json.load(scJsonFileHandle)
+            scJsonTargets = scJsonObject["targets"]
+            scSpritesList = []
+            for target in scJsonTargets:
+               isStage = target["isStage"]
+               name = target["name"]
+               variables = target["variables"]
+               lists = target["lists"]
+               broadcasts = target["broadcasts"]
+               blocks = target["blocks"]
+               scBlocksList = []
+               for blockID, block in blocks.items():
+                    if type(block) == Dict:
+                        opcode = block['opcode']
+                        nextID = block["next"]
+                        parentID = block["parent"]
+                        inputs = block["inputs"]
+                        topLevel = block["topLevel"]
+                        scBlocksList.append(scProjectBlock(blockID, opcode, nextID, parentID, inputs, topLevel))
+                    elif type(block) == List:
+                        pass
+               currentCostume = target["currentCostume"]
+               costumes = target["costumes"]
+               sounds = target["sounds"]
+               volume = target["volume"]
+               layerOrder = target["layerOrder"]
+               tempo = target["tempo"]
+               videoTransparency = target["videoTransparency"]
+               videoState = target["videoState"]
+               scSpritesList.append(scProjectSprite(isStage,name, variables, lists, broadcasts, scBlocksList, currentCostume, costumes, sounds, volume, layerOrder, tempo, videoTransparency, videoState))
+            monitors = scJsonObject["monitors"]
+            extensions = scJsonObject["extensions"]
+            return scProjectObject(scProjectTarget(scSpritesList), monitors, extensions)
+
+# standard compiler
+class blockASMStandard:
+    def __init__(self) -> None:
+        pass
+
+    def compile(self) -> Any:
+        pass
+
+#test function
+def testing():
+    import pathlib
+    import shutil
+    if pathlib.Path("./test/testproject").exists():
+        shutil.rmtree("./test/testproject")
+
+    obj = blockASMProject("./test/testproject.sb3", "./test/testproject").parseSb3Json()
+    logging.debug(obj.targets.sprites[0].currentCostume)
+testing()
